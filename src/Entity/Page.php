b271a36bb6292aaa24479cc890fdf1ede07ebd4d
Nikolay <himoshin@bk.ru>
2021-03-09 21:26:26 +0300

    Empty

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -9,42 +9,4 @@ use App\Traits\ArrayOrJsonResponse;
 final class Page
 {
     use ArrayOrJsonResponse;
-
-    /** @var int */
-    private $id;
-
-    /** @var string */
-    private $name;
-
-    /** @var string|null */
-    private $description;
-
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function updateName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function updateDescription(?string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
 }
